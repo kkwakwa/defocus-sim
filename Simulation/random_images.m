39590570adf66caa02d6337177fb51fae17265b8
Kwasi Kwakwa <kwasi.kwakwa@gmail.com>
2016-05-28 17:10:48 +0100

    slight modofocations to the random pattern generator

--- Simulation/random_images.m
+++ Simulation/random_images.m
@@ -1,4 +1,4 @@
-function random_images(pattern_number)
+function random_images(pattern_number, total_photons, photons,step)
   r2deg = 57.2957795;
   
   [z,NA,n0,n,n1,d0,d,d1,lamem,mag,focus,atf,ring,pixel,nn,be_res,al_res,pic]...
@@ -8,6 +8,8 @@ function random_images(pattern_number)
   
   theta = round(model.theta(pattern_number) * r2deg);
   phi = round(model.phi(pattern_number) * r2deg);
+  sidelen = (length(model.mask(:,1,1));
+  imlength = zeros(1,sidelen*sidelen);
   
   orig_filename = sprintf('Simulation.images/orientation-%i-%i.png',[theta;phi]);
   mask1 = model.mask(:,:,pattern_number) *...
