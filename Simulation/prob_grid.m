81a4137f32a1ce1359248c1cfd4c51f00d338717
Kwasi Kwakwa <kwasi.kwakwa@gmail.com>
2016-05-28 17:33:21 +0100

    probability function now working as expected

--- Simulation/prob_grid.m
+++ Simulation/prob_grid.m
@@ -1,11 +1,12 @@
-function imlength = prob_grid(pattern, sidelen)
+function imlength = prob_grid(pattern)
 
-rsum = 0
+rsum = 0;
+sidelen = size(pattern)(1);
 imlength = zeros(1,sidelen*sidelen);
 
   for i=1:sidelen
     for j=1:sidelen
-      rsum=rsum + model.mask(i,j,mapval);
+      rsum=rsum + pattern(i,j);
       rlen = j+((i-1)*sidelen);
       imlength(rlen) = rsum;
     end
